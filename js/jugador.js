d34c9cc953d2439c14894ddfc6a952dc92f7ef01
Chorizard <yaninalperez@gmail.com>
2019-02-21 20:50:43 -0300

    ciudad zombie

---
+++ js/jugador.js
@@ -0,0 +1,30 @@
+/* El objeto jugador es un objeto literal que se encuentra incompleto.
+ Solo tiene asignadas algunas de sus propiedades y ningun metodo */
+var Jugador = {
+  /* el sprite contiene la ruta de la imagen
+  */
+  sprite: 'imagenes/auto_rojo_abajo.png',
+  x: 130,
+  y: 160,
+  ancho: 15,
+  alto: 30,
+  velocidad: 10,
+  vidas: 5,
+  mover: function () {
+
+  },
+  perderVidas: function (cantVidas) {
+    this.vidas -= cantVidas;
+
+  },
+  
+
+  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
+  // y todo lo que haga falta para que cumpla con sus responsabilidades
+
+}
+
+/*imagenes/auto_rojo_abajo.png,
+  imagenes/auto_rojo_arriba.png,
+  imagenes/auto_rojo_derecha.png,
+  imagenes/auto_rojo_izquierda.png, */
